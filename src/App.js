17e2ce6290a42169ad2031bddfc984849741d68e
yuki nishimine <nisiyuu@yukinoMacBook-Pro.local>
2020-04-01 12:08:01 +0900

    about jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div><h1>hello world</h1></div>
-  );
+// function App() {
+//   return (
+//     <div><h1>hello world!!</h1></div>
+//   );
+// }
+
+class App extends Component{
+  render() {
+    return (
+      <React.Fragment>
+        <input type="text" onChange={() => { alert("aa") }}></input>
+      </React.Fragment>
+    )
+  }
 }
-
-export default App;
+  
+  export default App;
