541c9990f2cba420dd2f21d33acfa27cb595fc50
yuki nishimine <nisiyuu@yukinoMacBook-Pro.local>
2020-04-04 18:11:50 +0900

    provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './conponents/App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers';
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
