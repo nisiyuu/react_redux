14670db49cf4e91ae30eecdced97042b59e27149
yuki nishimine <nisiyuu@yukinoMacBook-Pro.local>
2020-04-05 16:29:58 +0900

    view

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux'//各reducerを結合する
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
+export default combineReducers({ events })
