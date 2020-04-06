45ad40761bcefcf74c7c914e2ce3482ff9807e2e
yuki nishimine <nisiyuu@yukinoMacBook-Pro.local>
2020-04-06 13:52:22 +0900

    create event

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { postEvents } from '../actions';
-// import { Link } from 'react-router-dom';//キャンセルボタン
+import { postEvent } from '../actions';
+import { Link } from 'react-router-dom';//キャンセルボタン
+import {Field, reduxForm} from 'redux-form'
 
 class EventsNew extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field
     return (
       <div>
-        hi
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span> }
       </div>
+    )
+  }
+
+  async onSubmit(values) {
+    console.log(values)
+    await this.props.postEvent(values);
+    this.props.history.push('/')
+  }
+
+  render() {
+    const { handleSubmit } = this.props
+
+    return (
+      //handleSubmitはredux-formの機能
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div><Field label="Title" name="title" type="text" component={this.renderField}></Field></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField}></Field></div>
+        <div>
+          <input type="submit" value="Submit" disabled={false}></input>
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
     )
   }
 }
 
-// const mapStateToProps = state => {
-//   return {
-//     events: state.events
-//   }
-// }
+const validate = values => {
+  const errors = {}
 
-// const mapDispachProps = dispatch => {
-//   return {
-//     postEvents: () => dispatch(postEvents())
-//   }
-// }
+  if (!values.title) errors.title = "Enter"
+  if (!values.body) errors.body = "Enter"  
+  return errors;
+}
 
-export default connect(null,null)(EventsNew);
+const mapDispatchToProps = {postEvent}
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({validate, form:'eventNewForm'})(EventsNew)
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'//各reducerを結合する
+import { reducer as form } from 'redux-form'//別名に変更
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events,form })
